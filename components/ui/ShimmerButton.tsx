aaa04c7b87a0011e40f8ad3350a1f370c45a993f
Kshitij Ohri <kshitijohri0101@gmail.com>
2025-02-01 05:21:55 +0530

    Starting of the portfolio
    Added some components and learned a couple of things.

---
+++ components/ui/ShimmerButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ShimmerButton = (
+  {title, icon, position, handleClick, otherClasses}:
+  { title :string; icon: React.ReactNode, position:string; handleClick?: () => void; otherClasses?:string;}
+) => {
+  return (
+    
+        // Button code
+        <button className={`inline-flex h-12 animate-shimmer items-center justify-center rounded-full border border-violet-400 bg-[linear-gradient(110deg,#000103,45%,#241e31,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors gap-2 ${otherClasses} `}>
+          {position === 'left' && icon}
+          {title}
+          {position === 'right' && icon}
+        </button>
+  )
+}
+
+export default ShimmerButton
