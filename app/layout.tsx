b6ee9bd482ff634f07a6f2643fd46646c0affdb8
Kshitij Ohri <kshitijohri0101@gmail.com>
2025-06-07 21:57:09 +0530

    text changes + Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="\AwardsAndCertifications\Deloitte.svg" type="image/png" sizes="32x32" />
+        <link rel="icon" href="\WebsiteIcon\K.svg" type="image/png" sizes="32x32" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
--- data/index.ts
+++ data/index.ts
@@ -51,8 +51,8 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently building a JS Animation library",
-      description: "The Inside Scoop",
+      title: "Currently Seeing what I can build next",
+      description: "It may be a game, a web app, or something else entirely.",
       className: "lg:col-span-3 md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
       titleClassName: "justify-center md:justify-start lg:justify-center",
